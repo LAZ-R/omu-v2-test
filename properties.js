1d9f410239a207fcbad77e92c5e8699f10e40ad1
LAZ-R <crazydrawer62@gmail.com>
2024-03-31 10:25:08 +0200

    v 0.1.7 - ajout photos legit accueil + fix desktop

--- properties.js
+++ properties.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Opale Makeup';
-export const APP_VERSION = '0.1.6'; // v 0.1.6 - ajout descriptions cours
+export const APP_VERSION = '0.1.7'; // v 0.1.7 - ajout photos legit accueil + fix desktop
 export const APP_SHORT_NAME = `omuapp001`;
--- sw.js
+++ sw.js
@@ -25,4 +25,4 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
-const version = '0.1.6';
+const version = '0.1.7';
